89a6bf8028ca128e62ef489d792adae64f724fa8
a catgirl <nyameowmeow@duck.com>
2025-03-11 13:24:44 -0400

    feat: user configurable keybindings :fire:

--- src/input.rs
+++ src/input.rs
@@ -9,6 +9,8 @@ impl Input {
     }
 
     pub fn blocking_wait_for_input(&self) -> SongControl {
+        let config = &crate::CONFIG.read().unwrap().keybinds;
+
         let chaw = self.0.getch().expect("can't read");
         match chaw {
             // TODO: arrow keys should be changed to respect hjkl
@@ -16,12 +18,26 @@ impl Input {
             Key::Down => SongControl::VolumeDown,
             Key::Left => SongControl::SeekBackward,
             Key::Right => SongControl::SeekForward,
-            Key::Char('r') => SongControl::ToggleLoop,
-            Key::Char('k') => SongControl::PrevSong,
-            Key::Char('j') => SongControl::NextSong,
-            Key::Char(' ') => SongControl::TogglePause,
-            Key::Char('s') => SongControl::ToggleShuffle,
-            Key::Ctrl('c') | Key::Char('q') => SongControl::DestroyAndExit,
+            Key::Char(key) => {
+                // TODO: more computer sciency approach lmao
+                // possibly something like .to_action() in TomlKeybinds or something
+                if key == config.toggle_loop {
+                    SongControl::ToggleLoop
+                } else if key == config.prev_song {
+                    SongControl::PrevSong
+                } else if key == config.next_song {
+                    SongControl::NextSong
+                } else if key == config.toggle_pause {
+                    SongControl::TogglePause
+                } else if key == config.toggle_shuffle {
+                    SongControl::ToggleShuffle
+                } else if key == config.exit {
+                    SongControl::DestroyAndExit
+                } else {
+                    SongControl::No
+                }
+            },
+            Key::Ctrl('c') => { SongControl::DestroyAndExit }
             _ => SongControl::No,
         }
     }
