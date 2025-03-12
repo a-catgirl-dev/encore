61e7b7a4822d209e20529a17cd73a96f31278d61
a catgirl <nyameowmeow@duck.com>
2025-03-12 19:49:14 -0400

    refactor: use HashMap for resolving inputs
    normally this would be perf:, but in this case, performance doesnt
    really matter here, and it was made to make the code less ugly (if else
    statement spam, what am i, yanderedev?).

--- src/input.rs
+++ src/input.rs
@@ -11,32 +11,23 @@ impl Input {
     pub fn blocking_wait_for_input(&self) -> SongControl {
         let config = &crate::CONFIG.read().unwrap().keybinds;
 
+        let mut keybind_map: std::collections::HashMap<char, SongControl> = std::collections::HashMap::new();
+        keybind_map.insert(config.toggle_loop, SongControl::ToggleLoop);
+        keybind_map.insert(config.prev_song, SongControl::PrevSong);
+        keybind_map.insert(config.next_song, SongControl::NextSong);
+        keybind_map.insert(config.toggle_pause, SongControl::TogglePause);
+        keybind_map.insert(config.toggle_shuffle, SongControl::ToggleShuffle);
+        keybind_map.insert(config.exit, SongControl::DestroyAndExit);
+
         let chaw = self.0.getch().expect("can't read");
+
         match chaw {
             // TODO: arrow keys should be changed to respect hjkl
             Key::Up => SongControl::VolumeUp,
             Key::Down => SongControl::VolumeDown,
             Key::Left => SongControl::SeekBackward,
             Key::Right => SongControl::SeekForward,
-            Key::Char(key) => {
-                // TODO: more computer sciency approach lmao
-                // possibly something like .to_action() in TomlKeybinds or something
-                if key == config.toggle_loop {
-                    SongControl::ToggleLoop
-                } else if key == config.prev_song {
-                    SongControl::PrevSong
-                } else if key == config.next_song {
-                    SongControl::NextSong
-                } else if key == config.toggle_pause {
-                    SongControl::TogglePause
-                } else if key == config.toggle_shuffle {
-                    SongControl::ToggleShuffle
-                } else if key == config.exit {
-                    SongControl::DestroyAndExit
-                } else {
-                    SongControl::No
-                }
-            },
+            Key::Char(key) => keybind_map.get(&key).cloned().unwrap_or(SongControl::No),
             Key::Ctrl('c') => { SongControl::DestroyAndExit }
             _ => SongControl::No,
         }
